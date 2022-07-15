d7de7d6751104e78a84d6f9e3f3b0ad386e83b02
StarGrim33 <prosky33@gmail.com>
2022-07-15 18:29:12 +0300

    2

--- Program.cs
+++ Program.cs
@@ -5,12 +5,12 @@ namespace JuniorHomeWork
     {
         static void Main(string[] args)
         {
-            Random numb = new Random();
-            int column = numb.Next(1, 10);
-            int rows = numb.Next(1, 10);
+            Random random = new Random();
+            int column = random.Next(1, 10);
+            int rows = random.Next(1, 10);
             int[,] numbers = new int[rows, column];
-            int sum = 0;
-            int multiplication = 0;
+            int sumSecondRow = 0;
+            int multiplication = 1;
             int minValue = 0;
             int maxValue = 50;
 
@@ -18,20 +18,36 @@ namespace JuniorHomeWork
             {
                 for (int j = 0; j < numbers.GetLength(1); j++)
                 {
-                    numbers[i, j] = numb.Next(minValue, maxValue);
+                    numbers[i, j] = random.Next(minValue, maxValue);
                     Console.Write(numbers[i, j] + " ");
-                    if (i == 1)
-                    {
-                        sum += numbers[i, j];
-                    }
-                    if (j == 0)
-                    {
-                        multiplication *= numbers[i, j];
-                    }
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine("Сумма чисел второй строки равна: " + sum);
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                while (i == 1)
+                {
+                    for (int j = 0; j < numbers.GetLength(1); j++)
+                    {
+                        sumSecondRow += numbers[i, j];
+                    }
+                    break;
+                }
+            }
+            Console.WriteLine("Сумма чисел второй строки равна: " + sumSecondRow);
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    while (j == 0)
+                    {
+                        multiplication *= numbers[i, j];
+                        break;
+                    }
+                }
+            }
             Console.WriteLine("Произведение чисел первого столбца равно: " + multiplication);
         }
     }
